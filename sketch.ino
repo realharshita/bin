dd8da366a2d806567cdec3784050b3fc3bbd5eb4
realharshita <slayyharshita@gmail.com>
2024-06-28 20:44:08 +0530

    changed project to start small

--- sketch.ino
+++ sketch.ino
@@ -1,11 +1,48 @@
-// Now turn this trash into treasure!
+const int ledPin = 2;
+const int buttonPin = 3;
+const int onboardLedPin = PICO_DEFAULT_LED_PIN;
+
+
+int buttonState = 0;
+
+int lastButtonState = HIGH;
+
+int ledState = LOW;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial1.begin(115200);
-  Serial1.println("Hello, Raspberry Pi Pico W!");
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(onboardLedPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  digitalWrite(onboardLedPin, LOW);
+
+
+
 }
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  delay(1); // this speeds up the simulation
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == LOW) {
+        ledState = !ledState;
+        digitalWrite(ledPin, ledState);
+        digitalWrite(onboardLedPin, ledState);
+
+
+      }
+    }
+  }
+  lastButtonState = reading;
+  
 }
